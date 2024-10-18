391692e1cc90a6852264121ac57a1191a4ee15ef
ParkMiram <125454927+ParkMiram@users.noreply.github.com>
2024-10-18 18:46:21 +0900

    [level 1] Title: 추억 점수, Time: 0.86 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,22 +1,11 @@
 function solution(name, yearning, photo) {
     const member = new Map();
 
-    for (let i = 0; i < name.length; i++) {
-        member.set(name[i], yearning[i]);
-    }
-
-    let score = 0;
-    const result = [];
-
-    photo.forEach(photo => {
-        for (let i = 0; i < photo.length; i++) {
-            if (member.get(photo[i]) !== undefined) score += member.get(photo[i]);
-            if (i === photo.length - 1) {
-                result.push(score);
-                score = 0;
-            }
-        }
+    name.forEach((el, i) => {
+        member.set(el, yearning[i]);
     })
+
+    const result = photo.map(el => el.reduce((score, mem) => score + (member.get(mem) || 0), 0));
 
     return result;
 }
